11c046f19a318427312a17a28d17d008d2b0528c
firnsy <firnsy@securixlive.com>
2011-06-30 15:58:27 +1000

    changed: tweaked usage.

--- src/util.c
+++ src/util.c
@@ -143,7 +143,7 @@ int DisplayBanner(void)
         "  ______   -*> Barnyard2 <*-\n"
         " / ,,_  \\  Version %s.%s.%s (Build %s)%s%s\n"
         " |o\"  )~|  By Ian Firns (SecurixLive): http://www.securixlive.com/\n"
-		" + '''' +  (C) Copyright 2008-2011 Ian Firns (SecurixLive).\n"
+		" + '''' +  (C) Copyright 2008-2011 Ian Firns <firnsy@securixlive.com>\n"
         "\n"
         , VER_MAJOR, VER_MINOR, VER_REVISION, VER_BUILD,
 #ifdef DEBUG

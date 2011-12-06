e763ebe0b2367f54cdfc6dfe8add8cdb41d99c2f
Eric Lauzon <beenph@gmail.com>
2011-12-06 18:25:05 -0500

    Fix issue 9 where the waldo file was using previously defined record index if the waldo file was not refereshed.
    Thanks to github user aval13

--- src/spooler.c
+++ src/spooler.c
@@ -439,6 +439,10 @@ int ProcessContinuous(const char *dirpath, const char *filebase,
                 continue;
             }
 
+	    spooler->record_idx = 0;
+            spoolerWriteWaldo(&barnyard2_conf->waldo, spooler);
+
+
             waiting_logged = 0;
 
             /* set timestamp to ensure we look for a newer file next time */

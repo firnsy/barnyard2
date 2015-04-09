e17f8789e0727f95f9e99a53e3037e163f40baa3
Pablo Cantos <pcantos@redborder.net>
2015-04-09 17:33:46 +0000

    rd_unified2.c modified: fixing macro and actionOfEvent

--- src/rbutil/rb_unified2.c
+++ src/rbutil/rb_unified2.c
@@ -35,6 +35,12 @@
 #include "unified2.h"
 #include <stdlib.h>
 
+#define U2_FLAG_ALLOWED 0x00        // impact_flag = 0 (packet allowed)
+#define U2_FLAG_BLOCKED 0x20        // impact_flag = 32 (packet dropped)
+#define U2_BLOCKED_FLAG_ALLOW 0x00  // blocked = 0 (packet allowed)
+#define U2_BLOCKED_FLAG_BLOCK 0x01  // blocked = 2 (packet dropped)
+#define U2_BLOCKED_FLAG_WOULD 0x02  // blocked = 3 (packet would have been dropped)
+#define U2_BLOCKED_FLAG_CANT  0x03  // blocked = 4 (packet cant be dropped)
 
 #define EVENT_IMPACT_FLAG(e) e->impact_flag
 #define EVENT_BLOCKED(e)     e->blocked
@@ -42,9 +48,12 @@
 /// @warning It's a macro, and it will return the caller function
 #define RETURN_ACTION_OF_EVENT(event) \
     do{ \
-        if(EVENT_IMPACT_FLAG(event)==0 && EVENT_BLOCKED(event)==0) return "alert";\
-        if(EVENT_IMPACT_FLAG(event)==32 && EVENT_BLOCKED(event)==1) return "drop";\
         if(event==NULL && event_type ==0) return "log";\
+        else if(EVENT_IMPACT_FLAG(event)==U2_FLAG_ALLOWED && EVENT_BLOCKED(event)==U2_BLOCKED_FLAG_ALLOW) return "alert";\
+        else if(EVENT_IMPACT_FLAG(event)==U2_FLAG_BLOCKED && EVENT_BLOCKED(event)==U2_BLOCKED_FLAG_BLOCK) return "drop";\
+        else if(EVENT_IMPACT_FLAG(event)==U2_FLAG_ALLOWED && EVENT_BLOCKED(event)==U2_BLOCKED_FLAG_WOULD) return "not dropped";\
+        else if(EVENT_IMPACT_FLAG(event)==U2_FLAG_ALLOWED && EVENT_BLOCKED(event)==U2_BLOCKED_FLAG_CANT) return "cant drop";\
+        else return NULL;\
     }while(0)
 
 const char *actionOfEvent(const void * voidevent,uint32_t event_type)
@@ -55,22 +64,30 @@ const char *actionOfEvent(const void * voidevent,uint32_t event_type)
             const Unified2IDSEvent_legacy *event = (const Unified2IDSEvent_legacy *)voidevent;
             RETURN_ACTION_OF_EVENT(event);
         }
+        break;
+        case UNIFIED2_IDS_EVENT_MPLS:
         case UNIFIED2_IDS_EVENT_VLAN:
         {
             const Unified2IDSEvent *event = (const Unified2IDSEvent *)voidevent;
             RETURN_ACTION_OF_EVENT(event);
         }
+        break;
         case UNIFIED2_IDS_EVENT_IPV6:
         {
             const Unified2IDSEventIPv6_legacy *event = (const Unified2IDSEventIPv6_legacy *)voidevent;
             RETURN_ACTION_OF_EVENT(event);
-
-        } 
+            }
+        break;
+        case UNIFIED2_IDS_EVENT_IPV6_MPLS:
         case UNIFIED2_IDS_EVENT_IPV6_VLAN:
         {
             const Unified2IDSEventIPv6 *event = (const Unified2IDSEventIPv6 *)voidevent;
             RETURN_ACTION_OF_EVENT(event);
         }
+        break;
+        default:
+            LogMessage("WARNING: actionOfEvent(): event_type inconsistent (%d)\n", event_type);
+        break;
     };
     return NULL;
 }
